37b09408909b7582c7fabf991295042b79801d29
lenadekraker <lenadekraker@hotmail.com>
2025-03-31 17:47:02 +0200

    I think it works now :) (added all classes)

--- newTriplification.py
+++ newTriplification.py
@@ -48,6 +48,16 @@ def normalize_country_name(name):
 globalEdu["Countries and areas"] = globalEdu["Countries and areas"].apply(normalize_country_name)
 valid_countries = set(globalEdu["Countries and areas"])  # Only allow these countries
 
+# Add all classes
+# Define classes explicitly
+g.add((SCHEMA.Country, RDF.type, SCHEMA.Class))
+g.add((EX.Population, RDF.type, SCHEMA.Class))
+g.add((EX.Education, RDF.type, SCHEMA.Class))
+g.add((EX.Health, RDF.type, SCHEMA.Class))
+g.add((EX.Economy, RDF.type, SCHEMA.Class))
+g.add((EX.Sector, RDF.type, SCHEMA.Class))
+g.add((EX.CompletionRate, RDF.type, SCHEMA.Class))
+
 
 # Loop through CSV Global Education and add triples
 globalEdu["Countries and areas"] = globalEdu["Countries and areas"].apply(normalize_country_name)
@@ -55,25 +65,39 @@ for _, row in globalEdu.iterrows():
     country_name = row["Countries and areas"].replace(" ", "_").replace(",", "") 
     country_uri = URIRef(EX + country_name)
 
+    g.add((country_uri, RDF.type, SCHEMA.Country))
+
     # Adding country, longitude and latitude
     g.add((country_uri, GN.name, Literal(row["Countries and areas"], datatype = XSD.string)))
     g.add((country_uri, GN.lat, Literal(row["Latitude "], datatype = XSD.double)))
     g.add((country_uri, GN.long, Literal(row["Longitude"], datatype = XSD.double)))
-    g.add((country_uri, ISO3["5.1"], Literal(row["Unemployment_Rate"], datatype=XSD.double)))
-    g.add((country_uri, EX.completionRatePrimaryFemale, Literal(row["Completion_Rate_Primary_Female"], datatype=XSD.double)))
-    g.add((country_uri, EX.completionRatePrimaryMale, Literal(row["Completion_Rate_Primary_Male"], datatype=XSD.double)))
-    g.add((country_uri, EX.completionRateLowerSecondaryFemale, Literal(row["Completion_Rate_Lower_Secondary_Female"], datatype =XSD.double)))
-    g.add((country_uri, EX.completionRateLowerSecondaryMale, Literal(row["Completion_Rate_Lower_Secondary_Male"], datatype =XSD.double)))
-    g.add((country_uri, EX.completionRateUpperSecondaryFemale, Literal(row["Completion_Rate_Upper_Secondary_Female"], datatype =XSD.double)))
-    g.add((country_uri, EX.completionRateUpperSecondaryMale, Literal(row["Completion_Rate_Upper_Secondary_Male"], datatype =XSD.double)))
-    g.add((country_uri, EX.outOfSchoolRatePrePrimaryFemale, Literal(row["OOSR_Pre0Primary_Age_Female"], datatype =XSD.double)))
-    g.add((country_uri, EX.outOfSchoolRatePrePrimaryMale, Literal(row["OOSR_Pre0Primary_Age_Male"], datatype =XSD.double)))
-    g.add((country_uri, EX.outOfSchoolRatePrimaryFemale, Literal(row["OOSR_Primary_Age_Female"], datatype =XSD.double)))
-    g.add((country_uri, EX.outOfSchoolRatePrimaryMale, Literal(row["OOSR_Primary_Age_Male"], datatype =XSD.double)))
-    g.add((country_uri, EX.outOfSchoolRateLowerSecondaryFemale, Literal(row["OOSR_Lower_Secondary_Age_Female"], datatype =XSD.double)))
-    g.add((country_uri, EX.outOfSchoolRateLowerSecondaryMale, Literal(row["OOSR_Lower_Secondary_Age_Male"], datatype =XSD.double)))
-    g.add((country_uri, EX.outOfSchoolRateUpperSecondaryFemale, Literal(row["OOSR_Upper_Secondary_Age_Female"], datatype =XSD.double)))
-    g.add((country_uri, EX.outOfSchoolRateUpperSecondaryMale, Literal(row["OOSR_Upper_Secondary_Age_Male"], datatype =XSD.double)))
+
+    economy_uri = URIRef(EX + country_name + "_Economy")
+    g.add((economy_uri, RDF.type, EX.Economy))
+    g.add((economy_uri, EX.hasEconomy, economy_uri))
+    g.add((economy_uri, ISO3["5.1"], Literal(row["Unemployment_Rate"], datatype=XSD.double)))
+    
+    completion_uri = URIRef(EX + country_name + "_CompletionRate")
+    g.add((completion_uri, RDF.type, EX.CompletionRate))
+    g.add((country_uri, EX.hasCompletionRate, completion_uri))  # Link to country   
+    g.add((completion_uri, EX.completionRatePrimaryFemale, Literal(row["Completion_Rate_Primary_Female"], datatype=XSD.double)))
+    g.add((completion_uri, EX.completionRatePrimaryMale, Literal(row["Completion_Rate_Primary_Male"], datatype=XSD.double)))
+    g.add((completion_uri, EX.completionRateLowerSecondaryFemale, Literal(row["Completion_Rate_Lower_Secondary_Female"], datatype =XSD.double)))
+    g.add((completion_uri, EX.completionRateLowerSecondaryMale, Literal(row["Completion_Rate_Lower_Secondary_Male"], datatype =XSD.double)))
+    g.add((completion_uri, EX.completionRateUpperSecondaryFemale, Literal(row["Completion_Rate_Upper_Secondary_Female"], datatype =XSD.double)))
+    g.add((completion_uri, EX.completionRateUpperSecondaryMale, Literal(row["Completion_Rate_Upper_Secondary_Male"], datatype =XSD.double)))
+    
+    out_of_school_uri = URIRef(EX + country_name + "_OutOfSchoolRate")
+    g.add((out_of_school_uri, RDF.type, EX.OutOfSchoolRate))
+    g.add((country_uri, EX.hasOutOfSchoolRate, out_of_school_uri))  # Link to country
+    g.add((out_of_school_uri, EX.outOfSchoolRatePrePrimaryFemale, Literal(row["OOSR_Pre0Primary_Age_Female"], datatype =XSD.double)))
+    g.add((out_of_school_uri, EX.outOfSchoolRatePrePrimaryMale, Literal(row["OOSR_Pre0Primary_Age_Male"], datatype =XSD.double)))
+    g.add((out_of_school_uri, EX.outOfSchoolRatePrimaryFemale, Literal(row["OOSR_Primary_Age_Female"], datatype =XSD.double)))
+    g.add((out_of_school_uri, EX.outOfSchoolRatePrimaryMale, Literal(row["OOSR_Primary_Age_Male"], datatype =XSD.double)))
+    g.add((out_of_school_uri, EX.outOfSchoolRateLowerSecondaryFemale, Literal(row["OOSR_Lower_Secondary_Age_Female"], datatype =XSD.double)))
+    g.add((out_of_school_uri, EX.outOfSchoolRateLowerSecondaryMale, Literal(row["OOSR_Lower_Secondary_Age_Male"], datatype =XSD.double)))
+    g.add((out_of_school_uri, EX.outOfSchoolRateUpperSecondaryFemale, Literal(row["OOSR_Upper_Secondary_Age_Female"], datatype =XSD.double)))
+    g.add((out_of_school_uri, EX.outOfSchoolRateUpperSecondaryMale, Literal(row["OOSR_Upper_Secondary_Age_Male"], datatype =XSD.double)))
 
 # Loop through CSV GDP and add triples
 gdp["country"] = gdp["country"].apply(normalize_country_name)
@@ -81,9 +105,11 @@ for _,row in gdp.iterrows():
     country_name = row["country"].replace(" ", "_").replace(",", "")
     if country_name in valid_countries:
         country_uri = URIRef(EX + country_name)
+        economy_uri = URIRef(EX + country_name + "_Economy")
+        g.add((economy_uri, EX.hasEconomy, economy_uri))
         
         # Adding GDP
-        g.add((country_uri, DP.grossDomesticProduct, Literal(row["gdp"], datatype = XSD.double)))
+        g.add((economy_uri, DP.grossDomesticProduct, Literal(row["gdp"], datatype = XSD.double)))
 
 
 # Loop through csv life expectancy + convertion to doubles (were strings before)
@@ -95,11 +121,13 @@ for _,row in lifeExp.iterrows():
     country_name = row["Country Name"].replace(" ", "_").replace(",", "")
     if country_name in valid_countries:
         country_uri = URIRef(EX + country_name)
+        economy_uri = URIRef(EX + country_name + "_Economy")
+        g.add((economy_uri, EX.hasEconomy, economy_uri))
 
         # Adding Health Expenditure, Education Expenditure and Corruption 
-        g.add((country_uri, EX.exependitureOnHealth, Literal(row["Health Expenditure %"], datatype = XSD.double)))
-        g.add((country_uri, EX.exependitureOnEducation, Literal(row["Education Expenditure %"], datatype = XSD.double)))
-        g.add((country_uri, ISO3["11.4"] , Literal(row["Corruption"], datatype = XSD.double)))
+        g.add((economy_uri, EX.exependitureOnHealth, Literal(row["Health Expenditure %"], datatype = XSD.double)))
+        g.add((economy_uri, EX.exependitureOnEducation, Literal(row["Education Expenditure %"], datatype = XSD.double)))
+        g.add((economy_uri, ISO3["11.4"] , Literal(row["Corruption"], datatype = XSD.double)))
 
 
 # Loop trough CSV agriculture sector
@@ -108,9 +136,12 @@ for _, row in agri.iterrows():
     country_name = row["Country Name"].replace(" ", "_").replace(",", "")
     if country_name in valid_countries:
         country_uri = URIRef(EX + country_name)
+        sector_uri = URIRef(EX + country_name + "_Sector")
+        g.add((sector_uri, RDF.type, EX.Sector))
+        g.add((country_uri, EX.hasSector, sector_uri))  # Link to country
 
         # Adding Industry Rate
-        g.add((country_uri, EX.agricultureRate, Literal(row["2023"], datatype = XSD.double)))     
+        g.add((sector_uri, EX.agricultureRate, Literal(row["2023"], datatype = XSD.double)))     
 
 
 # Loop trough CSV industry sector
@@ -119,9 +150,12 @@ for _, row in indu.iterrows():
     country_name = row["Country Name"].replace(" ", "_").replace(",", "")
     if country_name in valid_countries:
         country_uri = URIRef(EX + country_name)
+        sector_uri = URIRef(EX + country_name + "_Sector")
+        g.add((sector_uri, RDF.type, EX.Sector))
+        g.add((country_uri, EX.hasSector, sector_uri))  # Link to country
 
         # Adding Industry Rate
-        g.add((country_uri, EX.industryeRate, Literal(row["2023"], datatype = XSD.double))) 
+        g.add((sector_uri, EX.industryeRate, Literal(row["2023"], datatype = XSD.double))) 
 
 
 # Loop trough CSV service sector
@@ -130,9 +164,12 @@ for _, row in serv.iterrows():
     country_name = row["Country Name"].replace(" ", "_").replace(",", "")
     if country_name in valid_countries:
         country_uri = URIRef(EX + country_name)
+        sector_uri = URIRef(EX + country_name + "_Sector")
+        g.add((sector_uri, RDF.type, EX.Sector))
+        g.add((country_uri, EX.hasSector, sector_uri))  # Link to country
 
         # Adding Industry Rate
-        g.add((country_uri, EX.serviceRate, Literal(row["2023"], datatype = XSD.double)))        
+        g.add((sector_uri, EX.serviceRate, Literal(row["2023"], datatype = XSD.double)))        
 
 
 # Loop through the CSV Population
@@ -143,12 +180,19 @@ for _, row in popu.iterrows():
         country_uri = URIRef(EX + country_name)   
 
         # Adding population information
-        g.add((country_uri, EX.hasPopulationSize, Literal(row.loc["Population, total [SP.POP.TOTL]"], datatype = XSD.int)))
-        g.add((country_uri, EX.hasPopulationMaleSize, Literal(row.loc["Population, male (% of total population) [SP.POP.TOTL.MA.ZS]"], datatype = XSD.double)))
-        g.add((country_uri, EX.hasPopulationFemaleSize, Literal(row.loc["Population, female (% of total population) [SP.POP.TOTL.FE.ZS]"], datatype = XSD.double)))                                
-        g.add((country_uri, EX.hasSchoolPopulation, Literal(row.loc["Population ages 0-14, total [SP.POP.0014.TO]"], datatype = XSD.double)))
-        g.add((country_uri, EX.hasSchoolMalePopulation, Literal(row.loc["Population, male (% of population, ages 0-14)"], datatype = XSD.double)))
-        g.add((country_uri, EX.hasSchoolFemalePopulation, Literal(row.loc["Population, female (% of population, ages 0-14)"], datatype = XSD.double)))
+        population_uri = URIRef(EX + country_name + "_Population")
+        g.add((population_uri, RDF.type, EX.Population))
+        g.add((country_uri, EX.hasPopulation, population_uri))  # Link it to the country
+        g.add((population_uri, EX.hasPopulationSize, Literal(row.loc["Population, total [SP.POP.TOTL]"], datatype = XSD.int)))
+        g.add((population_uri, EX.hasPopulationMaleSize, Literal(row.loc["Population, male (% of total population) [SP.POP.TOTL.MA.ZS]"], datatype = XSD.double)))
+        g.add((population_uri, EX.hasPopulationFemaleSize, Literal(row.loc["Population, female (% of total population) [SP.POP.TOTL.FE.ZS]"], datatype = XSD.double)))  
+        
+        school_pop_uri = URIRef(EX + country_name + "_SchoolPopulation")
+        g.add((school_pop_uri, RDF.type, EX.SchoolPopulation))
+        g.add((country_uri, EX.hasSchoolPopulationData, school_pop_uri))  # Link to country                              
+        g.add((school_pop_uri, EX.hasSchoolPopulation, Literal(row.loc["Population ages 0-14, total [SP.POP.0014.TO]"], datatype = XSD.double)))
+        g.add((school_pop_uri, EX.hasSchoolMalePopulation, Literal(row.loc["Population, male (% of population, ages 0-14)"], datatype = XSD.double)))
+        g.add((school_pop_uri, EX.hasSchoolFemalePopulation, Literal(row.loc["Population, female (% of population, ages 0-14)"], datatype = XSD.double)))
 
 # Save the updated RDF graph
 output_ttl = "New triplified data.ttl"
